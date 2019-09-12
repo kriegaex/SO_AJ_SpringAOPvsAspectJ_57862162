4651776e069295faab3fabfd833818325d31ad33
Alexander Kriegisch <Alexander@Kriegisch.name>
2019-09-12 09:11:54 +0700

    Replace System.out.println in Application by logger output

--- src/main/java/com/spring/aspect/dynamicflow/Application.java
+++ src/main/java/com/spring/aspect/dynamicflow/Application.java
@@ -2,16 +2,20 @@ package com.spring.aspect.dynamicflow;
 
 import com.spring.aspect.dynamicflow.entity.JobContext;
 import com.spring.aspect.dynamicflow.process.JobProcess;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
+  private static final Logger log = LoggerFactory.getLogger(Application.class);
+
   public static void main(String[] args) {
     ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
     JobProcess jobProcess = context.getBean(JobProcess.class);
-    System.out.println("jobProcess = " + jobProcess);
+    log.info("jobProcess = {}", jobProcess);
     jobProcess.process(new JobContext());
   }
 }
